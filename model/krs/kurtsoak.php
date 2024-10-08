b1b1271e391965f48364b19f3e275a3ddeead5d3
AbyssWatcher <114407756+UnknownArtistt@users.noreply.github.com>
2024-10-08 08:55:00 +0200

    Update kurtsoak.php

--- model/krs/kurtsoak.php
+++ model/krs/kurtsoak.php
@@ -24,7 +24,7 @@ $result = $conn->query($sql);
 <body>
     <nav class="navbar">
         <ul>
-            <li><a href="../../crud/administrazioa.php">Hasiera</a></li> <!-- Hasierara joan -->
+            <li><a href="../../administrazioa.php">Hasiera</a></li> <!-- Hasierara joan -->
             <li><a href="../iks/ikasleak.php">Ikasleak</a></li> <!-- Ikasleen panelera joan -->
             <li><a href="../erb/erabiltzaileak.php">Erabiltzaileak</a></li> <!-- Erabiltzaileen panelera joan -->
             <li><a href="../ere/erregistro_eskaerak.php">Erregistro Eskaerak</a></li> <!-- Erregistro eskaeretara joan -->
